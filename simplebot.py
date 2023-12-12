e957216ff6ed690c318142fb3e6ce7830c7dfe4f
Marine2233 <138140093+Marine2233@users.noreply.github.com>
2023-12-12 13:17:53 +0300

    Add files via upload

--- simplebot.py
+++ simplebot.py
@@ -6,10 +6,6 @@ bot = telebot.TeleBot(API_TOKEN)
 
 global phone_book
 phone_book = {}
-""" phone_book = {
-        "дядя Ваня": {'pnones': 121212, 'email': "123@mail.ru"},
-        "дядя Вася": {'pnones': 55555}
-    } """
 
 def load_data():
     with open("phonebook.json", "r", encoding="utf-8") as fh:
@@ -19,6 +15,64 @@ def load_data():
 def save_data():
     with open("phonebook.json", "w", encoding="utf-8") as fh:
         fh.write(json.dumps(phone_book, ensure_ascii=False))
+
+def search(message):
+    surname = message.text
+
+    if surname in phone_book:
+        bot.send_message(message.from_user.id, f'{phone_book[surname]}')
+    else:
+        bot.send_message(message.from_user.id, f"Контакт {surname} не найден")
+
+def delete(message):
+    surname = message.text
+    if surname in phone_book:
+        del phone_book[surname]
+        bot.send_message(message.from_user.id, f"Контакт {surname} успешно удалён")
+    else:
+        bot.send_message(message.from_user.id, f"Контакт {surname} не найден")
+
+def set_newname(message):
+    name = message.text
+    phone_book[surname]['имя'] = name
+    bot.send_message(message.from_user.id, f"Имя контакта {surname} успешно изменено")    
+
+def replace_name(message):
+    global surname
+    surname = message.text
+    if surname in phone_book:
+        bot.send_message(message.from_user.id, "Введите новое имя:")
+        bot.register_next_step_handler(message, set_newname)
+    else:
+        bot.send_message(message.from_user.id, f"Контакт {surname} не найден")
+
+def set_newemail(message):
+    email = message.text
+    phone_book[surname]['почта'] = email
+    bot.send_message(message.from_user.id, f"Почта контакта {surname} успешно изменена")
+
+def replace_email(message):
+    global surname
+    surname = message.text
+    if surname in phone_book:
+        bot.send_message(message.from_user.id, "Введите новою почту:")
+        bot.register_next_step_handler(message, set_newemail)
+    else:
+        bot.send_message(message.from_user.id, f"Контакт {surname} не найден")
+
+def set_newphone(message):
+    phone = message.text    
+    phone_book[surname]['телефон'] = phone
+    bot.send_message(message.from_user.id, f"Телефон контакта {surname} успешно изменён")
+
+def replace_phone(message):
+    global surname
+    surname = message.text
+    if surname in phone_book:
+        bot.send_message(message.from_user.id, "Введите новый телефон:")
+        bot.register_next_step_handler(message, set_newphone)
+    else:
+        bot.send_message(message.from_user.id, f"Контакт {surname} не найден")                 
 
 def get_phone(message):
     global phone
@@ -56,7 +110,7 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь? Чтобы увидеть список команд, напиши /start")
     
     elif message.text == "/start":
-        bot.send_message(message.from_user.id, "Список команд: /contact, /save, /add, /delete, /search, /replace, /load")
+        bot.send_message(message.from_user.id, "Список команд: /contact, /save, /add, /delete, /search, /replace_name, /replace_email, /replace_phone, /load")
 
     elif message.text == "/load":                  
         load_data()
@@ -78,13 +132,24 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Контакты сохранены")
 
     elif message.text == "/search":        
-        bot.send_message(message.from_user.id, "Эта команда пока не реализована")
+        bot.send_message(message.from_user.id, "Введите фамилию:")
+        bot.register_next_step_handler(message, search)
 
-    elif message.text == "/replace":        
-        bot.send_message(message.from_user.id, "Эта команда пока не реализована")
+    elif message.text == "/replace_name":        
+        bot.send_message(message.from_user.id, "Введите фамилию:")
+        bot.register_next_step_handler(message, replace_name)
+
+    elif message.text == "/replace_email":        
+        bot.send_message(message.from_user.id, "Введите фамилию:")
+        bot.register_next_step_handler(message, replace_email)
+
+    elif message.text == "/replace_phone":        
+        bot.send_message(message.from_user.id, "Введите фамилию:")
+        bot.register_next_step_handler(message, replace_phone)        
 
     elif message.text == "/delete":        
-        bot.send_message(message.from_user.id, "Эта команда пока не реализована")                     
+        bot.send_message(message.from_user.id, "Введите фамилию:")
+        bot.register_next_step_handler(message, delete)                     
 
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
